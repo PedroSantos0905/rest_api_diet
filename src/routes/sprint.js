c04cbf3566cb13061c308a034970d3ae8bbdbe5b
PedroSantos0905 <pedrosantos0509@gmail.com>
2020-03-16 23:59:08 -0300

    Criação da PROCEDURE de adicionar usuários na sprint, e método de inserção da API

--- src/routes/sprint.js
+++ src/routes/sprint.js
@@ -31,4 +31,34 @@ router.put('/', auth.obrigatorio, (req, res, next) => {
     });
 });
 
+router.put('/adicionarUsuario', auth.obrigatorio, (req, res, next) => {
+  console.log(req.usuario)
+  mysql.getConnection((err, conn) => {
+      if (err) { return res.status(500).send({ error: error }) }
+
+      conn.query('SELECT * FROM usuario WHERE email = ?', [req.body.email], (error, results) => {
+        if (error) { return res.status(500).send({ error: error }) }
+        if (results.length <= 0) {
+            res.status(404).send({ mensagem: 'Usuário não cadastrado' })
+          } else {
+      conn.query(
+          `call pr_adiciona_usuario_sprint(?,?,?)`,
+          [req.usuario.id_usuario, req.body.id_sprint, req.body.email],
+          (error, field) => {
+              conn.release();
+              if (error) { return res.status(500).send({ error: error }) }
+              const response = {
+                  mensagem: 'Usuário adicionado com sucesso',
+                  usuarioAdicionado: {
+                      usuario: req.body.email,
+                      sprint: req.body.sprint
+                  }
+              }
+              return res.status(200).send(response);
+          })
+        }
+      })
+  });
+});
+
 module.exports = router;
